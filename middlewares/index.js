5aa3cfea67d65b6bbceceed711a39a1b65059db8
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-01-31 18:40:00 +0100

    views/layout

---
+++ middlewares/index.js
@@ -0,0 +1,17 @@
+/* copia del código de Ale */
+
+module.exports = isLoggedIn = (req, res, next) => {
+    if (!req.session.currentUser) {
+      res.redirect('/auth/login')
+    } else {
+      next()
+    }
+  };
+  
+  // module.exports = isAdmin = (req, res, next) => {
+  //   if (req.session.currentUser.role === 'admin') {
+  //     next()
+  //   } else {
+  //     res.redirect('/auth/login')
+  //   }
+  // };
